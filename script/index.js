f7955b90426730d0987367026cbdd6f20a9902cf
0sp1 <patrykszura0@gmail.com>
2025-10-15 22:36:07 +0200

    first commit

---
+++ script/index.js
@@ -0,0 +1,14 @@
+// console.log("hello, world!");
+// console.log("i hate you all");
+
+// window.alert("reterd");
+// window.alert("dog")
+// document.getElementById("myH1").textContent = "Hello";
+// document.getElementById("myP").textContent = "i hate you dog";
+
+let x;
+let y
+
+x = 500
+y = 44
+console.log(x, y);
