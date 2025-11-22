d0b07ea82398ba2ee75bc54fb8684045c2e58f9f
0sp1 <patrykszura0@gmail.com>
2025-11-22 16:29:52 +0100

    huh ?

--- script/index.js
+++ script/index.js
@@ -1,13 +1,14 @@
-// console.log("hello, world!");
-// console.log("i hate you all");
+var number = 5; // in-line comment 
+ 
+number = 9; 
+console.log(number - 2);
 
-// window.alert("reterd");
-// window.alert("dog")
-// document.getElementById("myH1").textContent = "Hello";
-// document.getElementById("myP").textContent = "i hate you dog";
+var myName = "Boue";
+let outName = "John";
+const pi = 3.14;
+console.log(myName, outName, pi);
 
-// let x = 500;
-// let y = 44;
-
-// console.log(`x is equal to ${x} and y is equal to ${y}`);
-
+myName = "Trist";
+outName = "Rober";
+// pi = "No name "
+console.log(myName, outName)
